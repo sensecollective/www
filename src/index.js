2ea512798cf965e21f397c4eeaaf023d9994b5eb
Rob Currie <robert.currie@gmail.com>
2017-09-14 18:02:02 -0700

    Cleanup formatting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
 ReactDOM.render(
@@ -11,4 +11,4 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
