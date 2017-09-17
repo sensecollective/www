c34a3957c60877185256c4ed18849ceb67b14418
Rob Currie <robert.currie@gmail.com>
2017-09-16 21:51:00 -0700

    Add google analytics

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
+import withTracker from './withTracker';
+
 import Home from "./containers/Home";
 import About from "./containers/About";
 import FAQ from "./containers/FAQ";
@@ -8,9 +11,9 @@ import NotFound from "./containers/NotFound";
 
 export default () =>
   <Switch>
-    <Route path="/" exact component={Home} />
-		<Route path="/about" exact component={About} />
-		<Route path="/faq" exact component={FAQ} />
-		<Route path="/contact" exact component={Contact} />
+    <Route path="/" exact component={withTracker(Home)} />
+		<Route path="/about" exact component={withTracker(About)} />
+		<Route path="/faq" exact component={withTracker(FAQ)} />
+		<Route path="/contact" exact component={withTracker(Contact)} />
 		<Route component={NotFound} />
   </Switch>;
