51dd7c9fafe2267aa6bc24a038226a3cc2b3800f
Rob Currie <robert.currie@gmail.com>
2017-10-14 10:03:03 -0700

    Switch to single Markdown component

--- src/Routes.js
+++ src/Routes.js
@@ -3,20 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 
 import withTracker from './withTracker';
 
+import Markdown from './components/Markdown';
+
 import Home from './containers/Home';
-import About from './containers/About';
-import FAQ from './containers/FAQ';
-import Resources from './containers/Resources';
-import Contact from './containers/Contact';
 import NotFound from './containers/NotFound';
 
 export default () => (
   <Switch>
     <Route path="/" exact component={withTracker(Home)} />
-    <Route path="/about" exact component={withTracker(About)} />
-    <Route path="/faq" exact component={withTracker(FAQ)} />
-    <Route path="/resources" exact component={withTracker(Resources)} />
-    <Route path="/contact" exact component={withTracker(Contact)} />
+    <Route path="/about" exact component={withTracker(Markdown)} />
+    <Route path="/faq" exact component={withTracker(Markdown)} />
+    <Route path="/resources" exact component={withTracker(Markdown)} />
+    <Route path="/contact" exact component={withTracker(Markdown)} />
     <Route component={NotFound} />
   </Switch>
 );
--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 
-export default class Contact extends Component {
+export default class Markdown extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
+      path: `${this.props.match.path.slice(1)}.md`,
       markdown: '',
     };
   }
 
   componentDidMount() {
-    fetch(props.name)
+    console.log(this.state.path);
+    fetch(this.state.path)
       .then(response => response.text())
       .then(markdown => this.setState({ markdown }))
       .catch(error => console.log(error));
@@ -19,8 +20,8 @@ export default class Contact extends Component {
 
   render() {
     return (
-      <div className="Contact">
-        <ReactMarkdown source={props.markdown} />
+      <div className="Markdown">
+        <ReactMarkdown source={this.state.markdown} />
       </div>
     );
   }
